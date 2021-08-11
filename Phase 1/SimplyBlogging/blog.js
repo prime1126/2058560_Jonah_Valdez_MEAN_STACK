3962ed6a63e03bace4e026c59cabfb2d1f0e7dbf
Jonah Valdez <valdezjonah111@gmail.com>
2021-08-11 16:48:53 -0500

    re-did phase 1 and got 100% working

--- Phase 1/SimplyBlogging/blog.js
+++ Phase 1/SimplyBlogging/blog.js
@@ -1,31 +1,13 @@
-var i = 0;
-const BtnAdd = document.querySelector(".btn-add");
-BtnAdd.addEventListener("click", addBlog);
+
 function addBlog() {
 
     var title = document.getElementById("title").value;
     var articles = document.getElementById("articles").value;
     var image = document.getElementById("addImage").value;
-    
     var newDiv = document.createElement('div');
+    newDiv.classList = "card"
     
-    
-    var postString = "<div styles: border=2px;>";
-    var t = title
-    var end ="</div>";
-    newDiv.className="col-3";
-    newDiv.id= i;
-    var xyz = postString+t+end;
-    alert(xyz);
-    document.getElementById("row").appendChild(newDiv);
-    
-
-
-    document.getElementById("col-3").innerHTML = xyz;
-    i = i+1;
-    //newDiv.classList.add('div-style')
-    //DivContainer.appendChild(newDiv);
-    
-    
-
+    var postString = "<div style= ><img src="+image+" style="+"width:100px;height:100px;"+"><div class="+"container"+
+    "><h4><b>"+title+"</b></h4><p>"+articles+"</p></div></div>";
+    document.getElementById("rowid").innerHTML += postString;
 }
